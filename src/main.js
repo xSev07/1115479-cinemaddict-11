868c4d3a46985a5b8e4c708a8c075a7bf510eb5e
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-05-29 02:31:45 +0500

    Add service worker

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import moment from "moment";
 import momentDurationFormatSetup from "moment-duration-format";
 
 momentDurationFormatSetup(moment);
-
 const pageController = new PageController();
 pageController.render();
+
+window.addEventListener(`load`, () => {
+  navigator.serviceWorker.register(`/sw.js`);
+});
